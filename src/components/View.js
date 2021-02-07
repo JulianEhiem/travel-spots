0418e4f431e7181fd3c77ed38cfa1d58101d6583
Julian Ehiem <jules.ehiem@gmail.com>
2021-02-06 20:39:17 -0500

    temporarily removed state

--- src/components/View.js
+++ src/components/View.js
@@ -1,53 +1,9 @@
-import { useState, useEffect } from "react";
 import style from '../styles/view.module.css';
 const View = () => {
-  const activateDropdown = () => {
-    const drop = document.querySelector("#dropdown");
-    drop.classList.toggle("show");
-  };
-  const [view, setView] = useState("Mountains");
-
-  useEffect(() => {
-    activateDropdown();
-  }, [view]);
-
   return (
     <div className={style.viewContainer}>
       <div className={style.dropdownDiv}>
-        <h1 className={style.viewType}>{view}</h1>
-        <button className={style.ddBtn} onClick={activateDropdown}>
-          <i class="fas fa-sort-down"></i>
-        </button>
-      </div>
-      <div className={style.dropdownContent} id="dropdown">
-        <a
-          href="#"
-          className= {style.dropdownContent}
-          onClick={() => setView("Beaches")}
-        >
-          Beaches
-        </a>
-        <a
-          href="#"
-          className={style.dropdownContent}
-          onClick={() => setView("Lakes")}
-        >
-          Lakes
-        </a>
-        <a
-          href="#"
-          className={style.dropdownContent}
-          onClick={() => setView("Plains")}
-        >
-          Plains
-        </a>
-        <a
-          href="#"
-          className={style.dropdownContent}
-          onClick={() => setView("Mountains")}
-        >
-          Mountains
-        </a>
+        <h1 className={style.viewType}>Mountains</h1>
       </div>
     </div>
   );
